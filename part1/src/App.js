f473b018d8ed812cbe9a37963b9a8572502967e0
Connor <connor.doherty97@gmail.com>
2022-09-22 20:46:51 -0400

    deleted unnecessary files and made two components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name} you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name = 'connor' age = '36'/>
+      <Hello name = 'chielu' age = {3*76}/>
+      <Hello name = 'eloi' age = {420}/>
     </div>
   );
 }
