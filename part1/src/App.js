e19d925548e9bcd20307107463637c6120e2a4d3
Connor <connor.doherty97@gmail.com>
2022-09-25 15:45:14 -0400

    basic componentry

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,51 @@
-function App (){
+const App = () => {
 
 
-  const now = new Date()
-  const a = 10
-  const b = 20
-
-  return(
-  <div>
-    <p>{now.toString()}</p>
-    <p>{a} plus {b} is {a+b}</p>
-  </div>
-)
+  function Header (props){
+    return(
+      <div>
+        <h1>{props.name}</h1>
+      </div>
+    )
   }
+
+  function Content (props){
+    return(
+      <div>
+        <p>{props.name}</p>
+        <p>{props.exercises}</p>
+      </div>
+    )
+  }
+  function Total (props){
+    return(
+      <div>
+        <p>{props.exercises.reduce((acc,c)=>acc+c,0)}</p>
+      </div>
+    )
+  }
+
+  const course = {
+    name:'Half Stack application development',
+    parts:[
+      {name:'fundamentals of react',
+    exercises:10},
+    {name:'using props to pass data',
+    exercises:7},
+    {name:'state of a component',
+    exercises:14}
+    ]
+  }
+
+  return (
+    <div>
+     <Header name = {course.name} />
+     <Content name = {course.parts[0].name} exercises = {course.parts[0].exercises}/>
+     <Content name = {course.parts[1].name} exercises = {course.parts[1].exercises}/>
+     <Content name = {course.parts[2].name} exercises = {course.parts[2].exercises}/>
+     <Total exercises = {[course.parts[0].exercises,course.parts[1].exercises,course.parts[2].exercises]} />
+    </div>
+  )
+}
 
 export default App
