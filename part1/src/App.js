7bd5dc879374dbd2b3b1732ee53cdb53a1f3b3a5
Connor <connor.doherty97@gmail.com>
2022-09-25 14:22:18 -0400

    fullstack open tut

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,82 +1,16 @@
-
-// const Hello = (props) => {
-//   return (
-//     <div>
-//       <p>Hello {props.name} you are {props.age} years old</p>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       <Hello name = 'connor' age = '36'/>
-//       <Hello name = 'chielu' age = {parts[2].exercises}/>
-//       <Hello name = 'pinhead' age = {Math.floor(Math.random()*100)}/>
-//     </div>
-//   );
-//
+function App (){
 
 
-//PRACTICE PROBLEM 
+  const now = new Date()
+  const a = 10
+  const b = 20
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course.name}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part.name}{props.part.exercises}</p>
-  )
-}
-
-const Content = (props) =>  {
-  return (
-    <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+  return(
+  <div>
+    <p>{now.toString()}</p>
+    <p>{a} plus {b} is {a+b}</p>
   </div>
-    
-  )
-}
-const Total = (props) => {
-  return (
-    <div>
-      <p>Num of exercises: {props.parts.reduce((acc,c)=> acc+c.exercises, 0)}</p>
-    </div>
-  )
-}
-
-function App () {
-
-    const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
+)
   }
-  ]
-}
-  return (
-    <div>
-    <Header course={course} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-  )
-}
 
 export default App
