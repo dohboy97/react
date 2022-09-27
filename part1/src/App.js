5f375d141763e125223287ec593a6dc846da035d
Connor <connor.doherty97@gmail.com>
2022-09-27 14:36:52 -0400

    stretching component tree

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,78 +1,28 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
-  function Part(props){
-    console.log(props.name)
-    return (
-      <div>
-        {props.name.map((el)=>{
-          return(
-          <p>Name:{el.name}, Exercises:{el.exercises}</p>
-          )
-        })}
-        
-      </div>
-    )
-  }
-
-  function Header (props){
-    
-    return(
-      <div>
-        <h1>{props.name.name}</h1>
-      </div>
-    )
-  }
-
-  function Content (props){
-    
-
-    return(
-      
-      <div>
-        <Part name = {props.part} />
-        </div>
-    )
-
+  const increaseByOne = () => {
    
-
-    
+    setCounter(counter + 1)
   }
 
-
-  function Total (props){
-    
-    return(
-      <div>
-        <p>{props.part.reduce((acc,c)=>acc+=c.exercises,0)}</p>
-      </div>
-    )
-  }
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  function eraseCounter(){
+    setCounter(0)
   }
 
   return (
     <div>
-     <Header name = {course} />
-     <Content part = {course.parts}/>
-     <Total part = {course.parts} />
+      <Display counter={counter}/>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick = {eraseCounter}>
+        zero
+      </button>
     </div>
   )
 }
-
 export default App
